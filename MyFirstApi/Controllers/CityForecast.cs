3eb72c064696f95740d0bba009832834ddb67b05
lauravoineag <77536595+lauravoineag@users.noreply.github.com>
2022-07-21 14:32:14 +0100

    Added MediaTR and separated project into Api, Domain and Data, created Command in GetWeatherForecast & changed Controller to incorporate Command Request with attribute From Query

--- MyFirstApi/Controllers/CityForecast.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstApi.Controllers;
-
-//ViewModel
-
-public class CityForecast
-{
-    public string City { get; set; }
-    public IEnumerable<WeatherForecast> WeatherForecasts { get; set; }
-}
